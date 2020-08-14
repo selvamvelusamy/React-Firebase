07c465cf8698570aeab4da699147aff51bdb86e3
selvamvelusamy <selvamveluchamy98422@gmail.com>
2020-08-14 14:39:38 +0530

    Store added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FireStore from "./Firebase/firebase";
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import FireStore from "./firebase/firebase";
 
 function App() {
-
-  useEffect(() => {
-    FireStore.collection('collection1').get()
-    .then(data => {
-      data.forEach(e => {
-        console.log(e.data());
-      })
-    });
-  }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		FireStore.collection("collection1")
+			.get()
+			.then((data) => {
+				data.forEach((e) => {
+					console.log(e.data());
+				});
+			});
+	}, []);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
